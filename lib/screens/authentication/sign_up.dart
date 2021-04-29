31bb28097a3a88e608443c8ccba04f8bc96b91c0
mohammedjinas <mohammedjinas7972@gmail.com>
2021-04-29 22:04:39 +0530

    sign in partly done

--- lib/screens/authentication/sign_up.dart
+++ lib/screens/authentication/sign_up.dart
@@ -5,39 +5,98 @@ class SignUp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Row(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Container(
-              margin: EdgeInsets.only(top: 180.0, left: 15.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Hello,",
-                    style: TextStyle(fontSize: 28.0),
+            Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: 180.0, left: 15.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Hello,",
+                        style: TextStyle(fontSize: 28.0),
+                      ),
+                      Text(
+                        "Welcome !",
+                        style: TextStyle(fontSize: 28.0),
+                      ),
+                    ],
                   ),
-                  Text(
-                    "Welcome !",
-                    style: TextStyle(fontSize: 28.0),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 160.0, left: 20.0),
+                  alignment: Alignment.center,
+                  child: SvgPicture.asset(
+                    'assets/images/signUp.svg',
+                    width: 200,
+                    height: 170,
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(30, 10, 30, 10),
+              child: TextField(
+                maxLength: 10,
+                decoration: InputDecoration(labelText: 'USN'),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 30, right: 30),
+              child: TextField(
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(20),
+              alignment: Alignment.center,
+              // ignore: deprecated_member_use
+              child: RaisedButton(
+                // style: OutlinedButton.styleFrom(primary: Colors.deepPurpleAccent),
+                padding: EdgeInsets.fromLTRB(50, 10, 50, 10),
+                color: Colors.deepPurpleAccent,
+                shape: RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(18.0),
+                ),
+                onPressed: () {
+                  //Navigator.push(context,
+                  // MaterialPageRoute(builder: (context) => SignUp()));
+                },
+                child: Text(
+                  "Sign in",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20),
+                ),
+              ),
+            ),
+            /*Container(
+              padding: EdgeInsets.only(left: 30),
+              child: ToggleButtons(
+                isSelected: [],
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Text('Student'),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Text('Teacher'),
                   ),
                 ],
               ),
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 160.0, left: 20.0),
-              alignment: Alignment.center,
-              child: SvgPicture.asset(
-                'assets/images/signUp.svg',
-                width: 200,
-                height: 170,
-              ),
-            ),
+            ),*/
           ],
         ),
-      ],
-    ));
+      ),
+    );
   }
 }
