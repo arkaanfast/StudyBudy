45963c0cd1faea4c24deedefd0d6bbe29429fad5
arkaanfast <4PA17CS011@pace.edu.in>
2021-06-12 12:54:08 +0530

    api integration plus fixes

--- lib/common_widget/sign_up_textfields.dart
+++ lib/common_widget/sign_up_textfields.dart
@@ -2,23 +2,41 @@ import 'package:flutter/material.dart';
 
 class SignUpTextFields extends StatelessWidget {
   final String inputText;
+  final TextEditingController controller;
+  final bool isPassword;
+  final TextEditingController pass;
 
-  SignUpTextFields(this.inputText);
+  SignUpTextFields({
+    @required this.inputText,
+    @required this.controller,
+    this.isPassword = false,
+    this.pass,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.only(top: 30.0),
       child: Container(
-        // decoration: //BoxDecoration(
-        //   border: Border.all(width: 2.0, color: Colors.grey),
-        // ),
         width: MediaQuery.of(context).size.width * 0.86,
         height: 45.0,
         child: TextFormField(
+          controller: this.controller,
+          validator: (value) {
+            if (value.isEmpty) {
+              return "Required";
+            }
+            if (this.isPassword && this.pass != null) {
+              if (this.controller.text != this.pass.text) {
+                return "Passwords dont match";
+              }
+            }
+            return null;
+          },
           cursorColor: Colors.grey,
           textAlign: TextAlign.center,
           keyboardType: TextInputType.text,
+          obscureText: isPassword ? true : false,
           decoration: InputDecoration(
             enabledBorder: OutlineInputBorder(
               borderSide: BorderSide(color: Colors.grey, width: 2.0),
@@ -34,7 +52,8 @@ class SignUpTextFields extends StatelessWidget {
             ),
             alignLabelWithHint: true,
             labelText: inputText,
-            labelStyle: TextStyle(fontWeight: FontWeight.bold, color: Colors.grey[400]),
+            labelStyle:
+                TextStyle(fontWeight: FontWeight.bold, color: Colors.grey[400]),
             floatingLabelBehavior: FloatingLabelBehavior.never,
           ),
         ),
--- lib/screens/authentication/sign_up.dart
+++ lib/screens/authentication/sign_up.dart
@@ -1,53 +1,71 @@
 import 'package:flutter/material.dart';
-import 'package:studybudy/models/sign_up_textfields.dart';
-import '../authentication/sign_in.dart';
+import 'package:studybudy/common_widget/sign_up_textfields.dart';
+import 'package:studybudy/screens/dashboard/student_dashboard.dart';
+import 'package:studybudy/services/auth_services.dart';
 
-class SignUp extends StatelessWidget {
-  final List<String> labelTexts = [
-    'U.S.N',
-    'Counselor',
-    'Phone No.',
-    'Email',
-  ];
+class SignUp extends StatefulWidget {
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  AuthService authService;
+
+  final _registerController = GlobalKey<FormState>();
+
+  final _firstName = TextEditingController();
+
+  final _lastName = TextEditingController();
+
+  final _usn = TextEditingController();
+
+  final _phoneNo = TextEditingController();
+
+  final _email = TextEditingController();
+
+  final _password = TextEditingController();
+
+  final _confirmPassword = TextEditingController();
+  final snackBar = SnackBar(content: Text('Already registered'));
+
+  @override
+  void initState() {
+    super.initState();
+    authService = AuthService();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
         child: Form(
+          key: _registerController,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Container(
                 padding: EdgeInsets.only(top: 40),
-                height: MediaQuery.of(context).size.height * 0.25,
-                child: Column(
-                  children: [
-                    Text(
-                      'Sign',
-                      style:
-                          TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      'Up',
-                      style:
-                          TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
-                    ),
-                  ],
+                child: Text(
+                  'Sign Up',
+                  style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
                 ),
               ),
-              Padding(
-                padding: EdgeInsets.only(top: 25.0),
+              Container(
+                padding: EdgeInsets.only(top: 60.0),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
                     Container(
-                      // decoration: BoxDecoration(
-                      //   border: Border.all(width: 2.0, color: Colors.grey),
-                      // ),
                       width: MediaQuery.of(context).size.width * 0.4,
                       height: 45.0,
                       child: TextFormField(
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return "Required";
+                          }
+                          return null;
+                        },
+                        controller: _firstName,
                         cursorColor: Colors.grey,
                         textAlign: TextAlign.center,
                         keyboardType: TextInputType.name,
@@ -63,18 +81,24 @@ class SignUp extends StatelessWidget {
                               borderRadius:
                                   BorderRadius.all(Radius.circular(0.0))),
                           labelText: 'First Name',
-                          labelStyle: TextStyle(fontWeight: FontWeight.bold , color: Colors.grey[400]),
+                          labelStyle: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey[400]),
                           floatingLabelBehavior: FloatingLabelBehavior.never,
                         ),
                       ),
                     ),
                     Container(
-                      // decoration: BoxDecoration(
-                      //   border: Border.all(width: 2.0, color: Colors.grey),
-                      // ),
                       width: MediaQuery.of(context).size.width * 0.4,
                       height: 45.0,
                       child: TextFormField(
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return "Required";
+                          }
+                          return null;
+                        },
+                        controller: _lastName,
                         textAlign: TextAlign.center,
                         autocorrect: false,
                         cursorColor: Colors.grey,
@@ -92,29 +116,33 @@ class SignUp extends StatelessWidget {
                                   BorderRadius.all(Radius.circular(0.0))),
                           floatingLabelBehavior: FloatingLabelBehavior.never,
                           labelText: 'Last Name',
-                          labelStyle: TextStyle(fontWeight: FontWeight.bold,color: Colors.grey[400]),
+                          labelStyle: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey[400]),
                         ),
                       ),
                     ),
                   ],
                 ),
               ),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 25.0),
-                child: ListView.builder(
-                  scrollDirection: Axis.vertical,
-                  shrinkWrap: true,
-                  itemBuilder: (ctx, index) {
-                    return SignUpTextFields(labelTexts[index]);
-                  },
-                  itemCount: labelTexts.length,
-                ),
+              SignUpTextFields(inputText: "Usn", controller: _usn),
+              SignUpTextFields(inputText: "Phone Number", controller: _phoneNo),
+              SignUpTextFields(inputText: "Email", controller: _email),
+              SignUpTextFields(
+                inputText: "Password",
+                controller: _password,
+                isPassword: true,
+              ),
+              SignUpTextFields(
+                inputText: "Confirm Password",
+                controller: _confirmPassword,
+                isPassword: true,
+                pass: _password,
               ),
               Container(
                 width: 160.0,
                 height: 70.0,
                 padding: EdgeInsets.only(top: 25.0),
-                // decoration: BoxDecoration(color: Color(0x3EC6FF)),
                 child: RaisedButton(
                   textColor: Colors.white,
                   shape: RoundedRectangleBorder(
@@ -125,12 +153,30 @@ class SignUp extends StatelessWidget {
                     style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                   ),
                   onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => SignIn(),
-                      ),
-                    );
+                    if (_registerController.currentState.validate()) {
+                      String userName = _firstName.text + " " + _lastName.text;
+
+                      authService
+                          .registerUser(
+                              username: userName,
+                              email: _email.text,
+                              password: _password.text,
+                              phoneNo: int.parse(_phoneNo.text),
+                              usn: _usn.text,
+                              isStudent: true)
+                          .then((response) {
+                        if (response == 201) {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => StudentDashboard(),
+                            ),
+                          );
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                        }
+                      });
+                    }
                   },
                 ),
               ),
